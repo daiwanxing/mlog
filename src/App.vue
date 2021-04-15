0b8673ed90337854926a57c07d5e3402f1344823
daiwanxing <daiwanxing@antvsion.com>
2021-04-15 13:33:34 +0800

    'initial'

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <hello-world msg="Hello Vue 3 + Vite" />
+</template>
+
+<script>
+  import helloWorld from '@/components/HelloWorld.vue';
+
+  export default {
+      name: "App",
+      components: {
+          helloWorld
+      }
+  }
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+const path = require('path');
+
+export default defineConfig({
+  server: {
+    port: 8090,
+    strictPort: true,
+    open: true,
+    proxy: {
+      '/api': {
+        target: "http://localhost:3001",
+        changeOrigin: true
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': path.join(__dirname, '/src')
+    }
+  },
+  plugins: [vue()]
+})
