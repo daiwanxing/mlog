4e253d1a03f2cb5db64ddc32584c56349ba54d62
daiwanxing <daiwanxing@antvsion.com>
2021-04-15 14:37:40 +0800

    update: dist

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue';
 const path = require('path');
 
 export default defineConfig({
-  base: '/mlog/',
+  base: '/mlog/dist/',
   server: {
     port: 8090,
     strictPort: true,
