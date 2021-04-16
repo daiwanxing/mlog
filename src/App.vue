06b534b2fabab79f3b7f061e586a4daf3f95808b
daiwanxing <daiwanxing@antvsion.com>
2021-04-16 13:17:59 +0800

    u: 新增Postcss.config.js配置

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/navigation.png" width="200"/>
-  <hello-world msg="Hello Vue 3 + Vite" />
-  <h1>Success start you web app!</h1>
-  <ul>
-    <li v-for="item in bannerList">
-      <img :src="item.imageUrl" :alt="item.typeTitle">
-    </li>
-  </ul>
+  <router-view></router-view>
 </template>
 
 <script>
-  import helloWorld from '@/components/HelloWorld.vue';
-  import { fetchBanner } from './api/index';
-  import { reactive, toRefs } from 'vue';
-
   export default {
-      name: "App",
-      components: {
-          helloWorld
-      },
-      // setup 不能设置为异步函数
-      setup () {
-        const data = reactive({
-            bannerList: []
-        });
-        fetchBanner().then(result => {
-            data.bannerList = result.banners || [];
-        });
-
-        return {
-            ...toRefs(data)
-        }
-      }
+      name: "netMusic-App"
   }
 </script>
 
 <style>
+:root {
+  --mainTheme: #d43c33;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+ul {
+  list-style: none;
 }
 </style>
---
+++ src/router/index.js
@@ -0,0 +1,26 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import { defineAsyncComponent } from 'vue';
+
+const index = () => import('@/components/index/index.vue');
+
+const routes = [
+    {
+        path: '/',
+        name: 'index',
+        redirect () {
+            return { name: '/mlog' }
+        }
+    },
+    {
+        path: '/mlog',
+        name: 'mlog',
+        component: index
+    }
+];
+
+const router = createRouter({
+    history: createWebHistory(), // 新的history方法取代了原来的mode属性
+    routes
+});
+
+export default router;
