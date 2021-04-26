91ac07afa46869a9ddb8dd245d9dd724c195a06a
daiwanxing <daiwanxing@antvsion.com>
2021-04-26 13:40:11 +0800

    update: 优化评论区展示

--- src/common/comment-list/comment-list.vue
+++ src/common/comment-list/comment-list.vue
@@ -6,23 +6,26 @@
         >
             <div class="comment-item-box">
                 <div class="comment-user">
-                    <div class="comment-user-avatar"></div>
+                    <div class="comment-user-avatar">
+                        <img :src="item.user.avatarUrl" alt="">
+                    </div>
                     <div class="comment-user-info">
-                         <span class="comment-user-name">{{item.user.nickname}}</span>
-                         <span class="comment-date"></span>
+                         <div class="comment-user-name">{{item.user.nickname}}</div>
+                         <div class="comment-date">{{formatDate(item.time)}}</div>
                          <!-- 脱离文档流 -->
                          <!-- <div class="comment-favor"></div> -->
                     </div>
-                    <div class="comment-desc">{{item.content}}</div>
                 </div>
+                <div class="comment-desc">{{item.content}}</div>
             </div>
         </li>
     </ul>
 </template>
 
 <script>
-import dayjs from '@/utils/dayjs-util';
 // 评论列表组件
+import dayjs from '@/utils/dayjs-util';
+
 export default {
     props: {
         comments: {
@@ -33,11 +36,57 @@ export default {
         }
     },
     setup() {
-        
+
+        function formatDate (data) {
+            return dayjs(data).fromNow();
+        }
+
+        return {
+            formatDate
+        }
     }
 }
 </script>
 
 <style scoped lang="scss">
+    .comment-list-box {
+        margin-top: 10px;
+        padding: 0 10px;
 
+        .comment-item-box {
+            display: flex;
+            flex-direction: column;
+            padding-bottom: 14px;
+
+            .comment-user {
+                display: flex;
+            }
+
+            .comment-user-avatar {
+                width: 32px;
+                height: 32px;
+                flex-shrink: 0;
+                object-fit: cover;
+                overflow: hidden;
+                border-radius: 50%;
+
+                img {
+                    width: 100%;
+                }
+            }
+
+            .comment-user-info {
+                flex: 1;
+                margin-left: 10px;
+                font-size: 12px;
+                color: #666;
+            }
+
+            .comment-desc {
+                padding-left: 42px;
+                color: #333;
+                font-size: 14px;
+            }
+        }
+    }
 </style>
--- src/utils/dayjs-util.js
+++ src/utils/dayjs-util.js
@@ -1,6 +1,8 @@
 import dayjs from 'dayjs/esm/index';
 import zhCn from 'dayjs/esm/locale/zh-cn';
+import relativeTime from 'dayjs/esm/plugin/relativeTime';
 
 dayjs.locale(zhCn);
+dayjs.extend(relativeTime)
 
 export default dayjs;
